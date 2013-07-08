3742235cc3fee5b958bc01341d6fb1423b22f0f2
pinpong <o.nitzschke@googlemail.com>
2013-07-08 20:30:55 +0200

    cpu/gpu overclocking

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -888,8 +888,8 @@ static void __init bus_init(const struct l2_level *l2_level)
 
 #ifdef CONFIG_USERSPACE_VOLTAGE_CONTROL
 
-#define USERCONTROL_MIN_VDD		 750
-#define USERCONTROL_MAX_VDD		1300
+#define USERCONTROL_MIN_VDD		 650
+#define USERCONTROL_MAX_VDD		1350
 #define NUM_FREQS			15
 
 ssize_t acpuclk_get_vdd_levels_str(char *buf) {
@@ -939,7 +939,7 @@ ssize_t acpuclk_set_vdd(char *buf) {
 #endif
 
 #ifdef CONFIG_CPU_FREQ_MSM
-static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
+static struct cpufreq_frequency_table freq_table[NR_CPUS][43];
 
 static void __init cpufreq_table_init(void)
 {
